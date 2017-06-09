2231dc946c61346009cecf1ccd9cb793111f61c0
lg <142>
2017-06-09 14:06:10 +0800

    back a demo

---
+++ softdoor/app/src/main/java/com/example/administrator/softdoor/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.administrator.softdoor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /**
+         * 本软件设想：
+         *
+         * 利用科大讯飞 声纹识别 和人脸识别技术 开启硬件门锁
+         *
+         * 实现开门目的
+         *
+         */
+    }
+}
